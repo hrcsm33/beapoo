4d2feb76dcc97c7f896ab770c95dd8e022144f97
choi <s.choi@nexpander.com>
2019-07-11 14:31:14 +0900

    first

---
+++ bookmark/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+# CharField	제한된 문자열 필드 타입. 최대 길이를 max_length 옵션에 지정해야 한다.
+# 문자열의 특별한 용도에 따라 CharField의 파생클래스로서, 이메일 주소를 체크를 하는 EmailField, IP 주소를 체크를 하는 GenericIPAddressField,
+# 콤마로 정수를 분리한 CommaSeparatedIntegerField, 특정 폴더의 파일 패스를 표현하는 FilePathField, URL을 표현하는 URLField 등이 있다.
+# TextField	대용량 문자열을 갖는 필드
+# IntegerField	32 비트 정수형 필드. 정수 사이즈에 따라 BigIntegerField, SmallIntegerField 을 사용할 수도 있다.
+# BooleanField	true/false 필드. Null 을 허용하기 위해서는 NullBooleanField를 사용한다.
+# DateTimeField	날짜와 시간을 갖는 필드. 날짜만 가질 경우는 DateField, 시간만 가질 경우는 TimeField를 사용한다.
+# DecimalField	소숫점을 갖는 decimal 필드
+# BinaryField	바이너리 데이타를 저장하는 필드
+# FileField	파일 업로드 필드
+# ImageField	FileField의 파생클래스로서 이미지 파일인지 체크한다.
+# UUIDField	GUID (UUID)를 저장하는 필드
+
+
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site url')
+
+    def __str__(self):
+        return self.site_name + "-" + self.url
---
+++ bookmark/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Bookmark
+from django.views.generic.list import ListView
+
+# 뷰에는 함수형 뷰클래스와 클래스형 뷰클스가있다
+class BookMarkListView(ListView):
+    """
+    ListView 디폴트 지정 속성
+    1) 컨텍스트 변수 : object_list
+    2) 템플릿 파일 : bookmark_list.html (모델명소문자_list.html)
+    함수형 뷰에서반복되는 공통 부분을 패턴화하여 사용하기 쉽게 추상화 해두었다.
+    아래와 같이 최소한 그 뷰가 어떤 모델을 사용할 것인지만 지정해주면 나머진 모두 상위 클래스(여기서는 List generic view)가 모든 걸 알아서 해준다.
+    뷰는 간단 명료해야 한다.
+    뷰 코드의 양은 적으면 적을수록 좋다.
+    뷰 안에서 같은 코드를 반복적으로 사용하지 않는다.
+    뷰는 프레젠테이션 로직에서 관리하고 비즈니스 로직은 모델에서 처리한다. 매우 특별한 경우에만 폼에서 처리한다.
+    403, 404, 500 에러 핸들링에는 CBV를 이용하지 않고 FBV를 이용한다.
+    믹스인은 간단명료해야 한다.
+    https://wikidocs.net/9623
+    """
+
+    model = Bookmark
+
